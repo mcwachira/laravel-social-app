f2e0302faddd814ae53d0ff2d1a8cad682ae74ce
mcwachira <mcwachira@outlook.com>
2025-12-23 15:36:51 +0300

    Created a one to many relationship between user and post models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -12,4 +13,8 @@ class Post extends Model
         'title',
         'body'
     ];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
