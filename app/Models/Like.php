469f1ffcdbf6da2a4c145e4bae341094b82160b5
mcwachira <mcwachira@outlook.com>
2026-01-07 13:56:07 +0300

    Made changes to the like model

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -2,21 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Like extends Model
 {
 
-    protected $fillable = ['post_id', 'user_id'];
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = ['likeable_id','likeable_type', 'user_id'];
 
-    public function post():BelongsTo
-    {
-        return $this -> belongsTo(Post::class);
-    }
 
     public function user():BelongsTo
     {
         return $this -> belongsTo(User::class);
+    }
+
+    public function likeable(): MorphTo
+    {
+        return $this->morphTo();
     }
 }
