9ff74866cece9013c3e426236d27d9c8e418f638
mcwachira <mcwachira@outlook.com>
2026-01-08 10:48:09 +0300

    updated the User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,13 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'is_private'=>fake()->boolean(20),
+            'is_verified'=>fake()->boolean(10),
         ];
     }
 
@@ -39,6 +42,14 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function verified():static
+    {
+
+        return $this->state(fn(array $attributes) => [
+            'is_verified' => true,
         ]);
     }
 }
